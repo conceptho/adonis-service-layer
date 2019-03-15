051d653ba4660df03387bd06c427b4dcc0ecc214
Sávio Serra <serra.santos.s@gmail.com>
2019-03-15 16:33:05 -0300

    Service now use Model.validationMessages

--- src/providers/ServiceLayerProvider.js
+++ src/providers/ServiceLayerProvider.js
@@ -2,16 +2,12 @@ const { ServiceProvider } = require('@adonisjs/fold');
 
 class ServiceLayerProvider extends ServiceProvider {
   register() {
-    // Making sure those dependencies are booted
-    const iocDependencies = ['Validator', 'Database'];
-    iocDependencies.forEach(dep => this.app.use(dep));
-
     this.app.bind('Conceptho/Middlewares/UseTransaction', () => require('../middlewares/UseTransaction'));
     this.app.bind('Conceptho/Middlewares/HeaderPagination', () => require('../middlewares/HeaderPagination'));
 
     this.app.bind('Conceptho/Models', () => {
       const Model = require('../models/Model');
-      Model._bootIfNotBooted();
+      Model.bootIfNotBooted();
 
       return { Model };
     });
