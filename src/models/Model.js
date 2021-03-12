0937eb75c8bb740331723d1854c8ebaf5a7272c9
joseslima <guislima47@gmail.com>
2021-03-12 12:12:15 -0300

    fix filterMapping

--- src/models/Model.js
+++ src/models/Model.js
@@ -1,5 +1,6 @@
 const DefaultSerializer = require('../serializers/DefaultSerializer')
 const { pick } = require('lodash')
+const { helper: helperFilterMapping } = require('./filter')
 
 module.exports = (AdonisModel, Validator) =>
   class Model extends AdonisModel {
@@ -87,23 +88,7 @@ module.exports = (AdonisModel, Validator) =>
     }
 
     static filterMapping (nameOperation) {
-      const operatorMapping = {
-        eq: '=',
-        neq: '<>',
-        gt: '>',
-        gte: '>=',
-        lt: '<',
-        lte: '<=',
-        like: 'LIKE',
-        in: 'IN',
-        nin: 'NOT IN',
-        between: 'BETWEEN',
-        nbetween: 'NOT BETWEEN'
-      }
-
-      const values = nameOperation.split(':')
-      const operationKey = values[1] ? values[1].toLowerCase().trim() : 'eq'
-      return ({ operation: operatorMapping[operationKey], name: values[0].trim() })
+      helperFilterMapping(nameOperation)
     }
     
     async undelete (transaction) {
